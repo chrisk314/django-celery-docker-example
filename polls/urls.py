bc0052e8fd1e3694b19b0c9f728ec9402905aaa1
Chris Knight <chrisk314@gmail.com>
2018-10-06 17:39:53 +0100

    Task progress update

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path("", views.index, name="index"),
+    path("check/<str:task_id>/", views.check, name="check"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from celery.result import AsyncResult
+from django.http import JsonResponse
+from django.shortcuts import render
+
+from .tasks import do_some_queries
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    res = do_some_queries.delay()
+    questions_count = res.get() if res.ready() else None
+    context = (
+        {"questions_count": questions_count}
+        if questions_count is not None
+        else {"task_id": res.task_id}
+    )
+    return render(request, "polls/index.html", context)
+
+
+def check(request, task_id):
+    task = AsyncResult(task_id)
+    return JsonResponse({"questions_count": task.get() if task.ready() else None})
